a11747219145b2b10035364371588e362e598f88
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-11-29 23:17:04 +0200

    add to hashing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,11 @@
 const { User } = require("../models/usersModel");
+const bcrypt = require("bcrypt");
 
 const userSignup = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = new User({ email, password });
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = new User({ email, password: hashedPassword });
 
   try {
     await user.save();
@@ -12,7 +15,11 @@ const userSignup = async (req, res, next) => {
     }
     next(error);
   }
-  return res.status(201).json({ user });
+  return res.status(201).json({
+    user: {
+      _id: user._id,
+    },
+  });
 };
 
 const getAllUsers = async (req, res, next) => {
